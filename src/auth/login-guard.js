94c192d2435b06a188c7d0fd041577454b5bb0ae
Howard <62272939+juyoungIt@users.noreply.github.com>
2025-10-20 20:51:25 +0900

    fix[#20]: 로그인 시 새로고침 해야 우측상단 회원 프로필 이미지 보이는 현상 수정

--- src/auth/login-guard.js
+++ src/auth/login-guard.js
@@ -1,18 +1,6 @@
 (async () => {
-    try {
-        // 1. /members/me API를 호출하여 세션 유효성 검사
-        const response = await fetch('http://localhost:8080/members/me', {
-            credentials: 'include' // [필수] 쿠키를 요청에 포함시킴
-        });
-
-        // 2. [로직 반전]
-        //    응답이 200 OK이면 (로그인 된 상태)
-        if (response.ok) {
-            // 메인 페이지로 튕겨낸다.
-            window.location.replace('/pages/posts.html');
-        }
-    } catch (error) {
-        // 4. (예상된 실패)
-        // 아무것도 하지 않는다.
+    const email = sessionStorage.getItem('email');
+    if (email) {
+        window.location.replace('/pages/posts.html');
     }
 })();
